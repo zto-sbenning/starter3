98040ce554c6aad1e01b95e3f5e37b017d383681
samuel <samuel.benning@zento.fr>
2019-01-25 20:07:14 +0100

    test

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -19,7 +19,7 @@ import { SharedModule } from '../shared/shared.module';
     HomePageComponent,
     AboutPageComponent,
     NotFoundPageComponent,
-    Blah
+    blah
   ]
 })
 export class PagesModule { }
