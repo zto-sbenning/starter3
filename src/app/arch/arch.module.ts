d47a0a13a3b35e0b1eacd9f7739228487128d76e
samuel <samuel.benning@zento.fr>
2019-01-25 20:19:24 +0100

    arch

---
+++ src/app/arch/arch.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+/**
+ * Architectural module
+ */
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  declarations: []
+})
+export class ArchModule { }
